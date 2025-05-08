5c949bfa7bfb4ab0005c0dc616808cdedbb1404c
rene-nowo <renenowo98@gmail.com>
2025-05-08 09:39:13 +0200

    srun command

--- anura_manifest.sh
+++ anura_manifest.sh
@@ -6,3 +6,6 @@ python scripts/anuraset_manifest.py \
   --n-split 5 \
   --seed 1612 \
   --limit-fraction 0.05
+
+
+srun \  --job-name=anura_Yamnet\  --partition=A100-IML \  --cpus-per-task=32 \  --mem=200G \  --time=2-00:00:00 \  --container-image=/enroot/nvcr.io_nvidia_tensorflow_23.10-tf2-py3.sqsh \  --container-workdir="`pwd`" \  --container-mounts="`pwd`":"`pwd`",/netscratch/$USER/results:"$(pwd)"/results,/ds:/ds:ro \  bash -c "pip install -r requirements.txt && python main_classifier.py  --model yamnet --dataset_path /ds/audio/Bioacoustics/UAnuraSet --output_file_path /netscratch/$USER/results/classifier_results"
