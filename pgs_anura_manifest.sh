0f00763381a07c578d6c6da99ab3bd54e49c49be
rene-nowo <renenowo98@gmail.com>
2025-05-14 10:45:45 +0200

    updated the srun .sh file

---
+++ pgs_anura_manifest.sh
@@ -0,0 +1,21 @@
+srun \
+  --job-name=anura_Animal2Vec \
+  --partition=A100-IML \
+  --cpus-per-task=32 \
+  --mem=200G \
+  --time=0-08:00:00 \
+  --mail-type=ALL \
+  --mail-user=renenowo98@gmail.com \
+  --container-image=/enroot/nvcr.io_nvidia_pytorch_22.06-py3.sqsh.packages:torch==1.13.0a0+340c412 \
+  --container-workdir="$(pwd)" \
+  --container-mounts="$(pwd):$(pwd),/netscratch/$USER/results:$(pwd)/results,/ds:/ds:ro" \
+  --wrap "python -m pip install --quiet --upgrade pip && \
+          pip install --quiet -r requirements.txt && \
+          python scripts/anuraset_manifest.py \
+            /ds/audio/Bioacoustics/AnuraSet/anuraset \
+            --audio-dir audio \
+            --dest manifests/validation \
+            --valid-percent 0.2 \
+            --n-split 5 \
+            --seed 1612 \
+            --limit-fraction 0.05"
