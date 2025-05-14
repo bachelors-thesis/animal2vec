84dfb30854bfb9f6ce9bc66f619a3ffbf58d4944
rene-nowo <renenowo98@gmail.com>
2025-05-14 10:53:54 +0200

    corrected the container img pull

--- pgs_anura_manifest.sh
+++ pgs_anura_manifest.sh
@@ -1,12 +1,13 @@
 srun \
   --job-name=anura_Animal2Vec \
   --partition=A100-IML \
+  --gpus=1 \
   --cpus-per-task=32 \
   --mem=200G \
   --time=0-08:00:00 \
   --mail-type=ALL \
   --mail-user=renenowo98@gmail.com \
-  --container-image=/enroot/nvcr.io_nvidia_pytorch_22.06-py3.sqsh.packages:torch==1.13.0a0+340c412 \
+  --container-image=/enroot/nvcr.io_nvidia_pytorch_22.06-py3.sqsh \
   --container-workdir="$(pwd)" \
   --container-mounts="$(pwd):$(pwd),/netscratch/$USER/results:$(pwd)/results,/ds:/ds:ro" \
   bash -c "python -m pip install --quiet --upgrade pip && \
