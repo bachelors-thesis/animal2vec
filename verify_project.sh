55f517bfbba58c887e6af795f722a5a69542cab6
rene-nowo <renenowo98@gmail.com>
2025-05-22 16:21:17 +0200

    changed the reqs.txt file

---
+++ verify_project.sh
@@ -0,0 +1,10 @@
+srun \  
+--job-name=anura_Yamnet\  
+--partition=A100-IML \  
+--cpus-per-task=32 \  
+--mem=200G \  
+--time=2-00:00:00 \  
+--container-image=/enroot/nvcr.io_nvidia_tensorflow_23.10-tf2-py3.sqsh \  
+--container-workdir="`pwd`" \  
+--container-mounts="`pwd`":"`pwd`",/netscratch/$USER/results:"$(pwd)"/results,/ds:/ds:ro \  
+bash -c "pip install -r requirements.txt && python main_classifier.py  --model yamnet --dataset_path /ds/audio/Bioacoustics/UAnuraSet --output_file_path /netscratch/$USER/results/classifier_results"
