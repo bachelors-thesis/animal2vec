51a74783b3f1b492f2740e822d9f64d594e6e40b
rene-nowo <renenowo98@gmail.com>
2025-05-22 16:26:22 +0200

    changed the reqs.txt file

--- verify_project.sh
+++ verify_project.sh
@@ -1,10 +1,11 @@
 srun \  
---job-name=anura_Yamnet\  
+--job-name=anura_Animal2Vec \  
 --partition=A100-IML \  
 --cpus-per-task=32 \  
 --mem=200G \  
 --time=2-00:00:00 \  
---container-image=/enroot/nvcr.io_nvidia_tensorflow_23.10-tf2-py3.sqsh \  
+--gpus=1 \  
+--container-image=/enroot/nvcr.io_nvidia_pytorch_22.06-py3.sqsh \  
 --container-workdir="`pwd`" \  
 --container-mounts="`pwd`":"`pwd`",/netscratch/$USER/results:"$(pwd)"/results,/ds:/ds:ro \  
-bash -c "pip install -r requirements.txt && python main_classifier.py  --model yamnet --dataset_path /ds/audio/Bioacoustics/UAnuraSet --output_file_path /netscratch/$USER/results/classifier_results"
+bash -c "pip install -r requirements.txt && python testing_cuda.py"
