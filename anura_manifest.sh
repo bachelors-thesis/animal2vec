4c7059a7f5c5014aea20bb2f83cc107723f2478e
rene-nowo <renenowo98@gmail.com>
2025-06-23 13:23:02 +0200

    working version

--- anura_manifest.sh
+++ anura_manifest.sh
@@ -1,8 +1,8 @@
 python scripts/anuraset_manifest.py \
   ../anuraset/anuraset \
   --audio-dir wav8k \
-  --dest manifests/validation \
+  --dest manifests_8k/anuraset_100pct/validation \
   --valid-percent 0.2 \
   --n-split 5 \
   --seed 1612 \
-  --limit-fraction 0.05
+  --limit-fraction 1.0
